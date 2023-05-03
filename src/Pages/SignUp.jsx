3ab714aecf13922935354d4f47713692e86991e8
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2023-05-03 20:52:44 +0530

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -123,7 +123,7 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/login" variant="body2">
+                <Link style={{cursor:"pointer"}} onClick={()=>navigate("/login")} variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
